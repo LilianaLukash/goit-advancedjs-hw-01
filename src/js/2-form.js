c988a354c737e2a7cdae144a8147016e688d374f
Liliana <liliana.ubisoft@gmail.com>
2024-07-22 15:37:16 +0100

    2

---
+++ src/js/2-form.js
@@ -0,0 +1,51 @@
+const feedbackFormEl = document.querySelector('.js-feedback-form');
+let formData = {};
+
+const fillFormFields = form => {
+  const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (formDataFromLS === null) {
+    return;
+  }
+
+  formData = formDataFromLS;
+
+  for (const key in formDataFromLS) {
+    if (formDataFromLS.hasOwnProperty(key)) {
+      form.elements[key].value = formDataFromLS[key];
+    }
+  }
+};
+
+fillFormFields(feedbackFormEl);
+
+const onFormFieldInput = event => {
+  // const {
+  //   target: { value: fieldValue },
+  // } = event;
+
+  const fieldName = event.target.name;
+  const fieldValue = event.target.value;
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+    event.preventDefault();
+    
+
+const inputs = feedbackFormEl.querySelectorAll('input');  // Предполагается, что класс '.feedback-form-input' убран для упрощения
+  for (const input of inputs) {
+    if (input.value.trim() === '') {  
+      alert('Заповніть всі поля!');
+      return;  
+    }
+    }
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldInput);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
